d68d3db36560b64c76eb9031044ac8c21e0037ef
2Kelvin <kvin.mwangi254@gmail.com>
2022-05-04 17:53:27 +0300

    added useState hook

--- Content.js
+++ Content.js
@@ -1,37 +1,20 @@
+import { useState } from "react";
+
 const Content = () => {
+    const [planet, setPlanet] = useState('Earth');
+
     const handlePlanetChange = () => {
-        const planets = ["Mars", "Earth", "Venus", "Jupiter"];
-        const planetIndex = Math.floor(Math.random() * 4);
-        return planets[planetIndex];
+        const planets = ["Mars", "Earth", "Venus", "Jupiter", "Mercury", "Neptune"];
+        const planetIndex = Math.floor(Math.random() * planets.length);
+        setPlanet(planets[planetIndex]);
     };
-
-    // button 'Click Me'
-    const handleClick = () => console.log("You clicked me");
-    // button 'Name Planet'
-    const handleNameClick = planet => console.log(`You clicked ${planet}`);
-    // button 'So Eventful'
-    const handleEventClick = e => console.log(e.target.innerText);
-    // function to handle double clicks, like on the 'main > p' below
-    const handleDoubleClicks = () => console.log("You double clicked me, yes?");
-
-    // const contentCSS = {
-    //     fontWeight: "700",
-    //     color: "magenta",
-    //     textDecoration: "underline"
-    // };
-    // <span style={contentCSS}>{handlePlanetChange()}</span> ->> this was for the code commented above
 
     return (
         <main>
-            <p onDoubleClick={handleDoubleClicks}>
-            Welcome to {handlePlanetChange()}
-            </p>
+            <p> Welcome to {planet} </p>
             <br></br>
-            <button onClick={handleClick}>Click Me</button>
-            <br></br>
-            <button onClick={() => handleNameClick("Venus")}>Name Planet</button>
-            <br></br>
-            <button onClick={(e) => handleEventClick(e)}>So Eventful</button>
+
+            <button onClick={handlePlanetChange}>Switch Planet</button>
         </main>
     );
 }
