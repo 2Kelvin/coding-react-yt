9f23d4583511c9c787efa4963a5c29fe74b8d84b
2Kelvin <kvin.mwangi254@gmail.com>
2022-05-04 13:38:49 +0300

    added button click events

--- Content.js
+++ Content.js
@@ -1,9 +1,19 @@
 const Content = () => {
-    // const handlePlanetChange = () => {
-    //     const planets = ["Mars", "Earth", "Venus", "Jupiter"];
-    //     const planetIndex = Math.floor(Math.random() * 4);
-    //     return planets[planetIndex];
-    // }
+    const handlePlanetChange = () => {
+        const planets = ["Mars", "Earth", "Venus", "Jupiter"];
+        const planetIndex = Math.floor(Math.random() * 4);
+        return planets[planetIndex];
+    };
+
+    // button 'Click Me'
+    const handleClick = () => console.log("You clicked me");
+    // button 'Name Planet'
+    const handleNameClick = planet => console.log(`You clicked ${planet}`);
+    // button 'So Eventful'
+    const handleEventClick = e => console.log(e.target.innerText);
+    // function to handle double clicks, like on the 'main > p' below
+    const handleDoubleClicks = () => console.log("You double clicked me, yes?");
+
     // const contentCSS = {
     //     fontWeight: "700",
     //     color: "magenta",
@@ -13,9 +23,15 @@ const Content = () => {
 
     return (
         <main>
-            <p>
-            Welcome to planet 
+            <p onDoubleClick={handleDoubleClicks}>
+            Welcome to {handlePlanetChange()}
             </p>
+            <br></br>
+            <button onClick={handleClick}>Click Me</button>
+            <br></br>
+            <button onClick={() => handleNameClick("Venus")}>Name Planet</button>
+            <br></br>
+            <button onClick={(e) => handleEventClick(e)}>So Eventful</button>
         </main>
     );
 }
